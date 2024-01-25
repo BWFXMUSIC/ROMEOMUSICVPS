adeb889801c2b8fc440ab0152d8f123df265aebf
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-01-25 10:45:22 +0530

    Update logging.py

--- Romeo/logging.py
+++ Romeo/logging.py
@@ -1,3 +1,14 @@
+# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
+# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
+
+""""
+TheTeamAlexa is a project of Telegram bots with variety of purposes.
+Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
+
 import logging
 from logging.handlers import RotatingFileHandler
 
@@ -8,15 +19,15 @@ logging.basicConfig(
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
-        RotatingFileHandler(
-            LOG_FILE_NAME, maxBytes=5000000, backupCount=10
-        ),
+        RotatingFileHandler(LOG_FILE_NAME, maxBytes=5000000, backupCount=10),
         logging.StreamHandler(),
     ],
 )
 
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
+logging.getLogger("ntgcalls").setLevel(logging.ERROR)
+logging.getLogger("httpx").setLevel(logging.ERROR)
 
 
 def LOGGER(name: str) -> logging.Logger:
